94a2d6d561a3e301f44aec4bc7e5eb0fd0cb073a
Mike Panuelos <mike.panuelos01@utrgv.edu>
2022-09-30 10:53:32 -0500

    minor css

--- frontend/src/pages/Server.jsx
+++ frontend/src/pages/Server.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Server = () => {
   return (
     <>
-        <section className='container md:max-w-[1240px] text-white mx-auto mt-8'>
+        <section className='container md:max-w-[1240px] text-white mx-auto mt-8 p-4'>
             <div>Create a Server</div>
             <div>Join a Server</div>
         </section>
