aa2092efbcfb250fd7ea4504abd142f03f712446
Robert Chapa <robert.chapa8520@gmail.com>
2022-10-12 13:30:04 -0500

    config

--- backend/controllers/MafiaController.js
+++ backend/controllers/MafiaController.js
@@ -1,25 +1,41 @@
 const asynchHandler = require('express-async-handler')
 
+const mod = require('../models/modModel')
+
 // @desc    Get Server with serverCode
 // @route   Get /gamenight/server/mafia/:id
 // @access  Public
-const getServer = (req, res) => {
-    res.status(200).json({message: `Server Code is: ${req.params.id}`})
-}
+const getServer = asyncHandler(async (req, res) => {
+    const server = await mod.find()
+    res.status(200).json(server)
+})
 
 // @desc    Create Server 
 // @route   Post /gamenight/server/mafia
 // @access  Public
-const createServer = (req, res) => {
-    res.status(200).json({message: 'Created Server with server code 123456'})
-}
+const createServer = asyncHandler(async (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('was empty')
+    }
+
+    const server = await mod.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(server)
+})
 
 // @desc    Delete Server with serverCode
 // @route   Delete /gamenight/server/mafia/:id
 // @access  Public
-const deleteServer = (req, res) => {
+const deleteServer = asyncHandler(async (req, res) => {
+
+    const server = await mod.findById(req.params.id);
+    await mod.remove()
+
     res.status(200).json({message: `Deleted Server with server code: ${req.params.id}` })
-}
+})
 
 module.exports = {
     getServer, createServer, deleteServer
