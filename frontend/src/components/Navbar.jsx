7a55b74d421a268ca7e0eaa858f3759fa108c5cf
Gerardo Garza <owlscloud@gmail.com>
2022-10-12 13:50:07 -0500

    desktop nav view

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <>
       <div
-        className="md:hidden mr-4 p-2 justify-start text-white"
+        className="md:hidden mr-4 p-2 pb-0 justify-start text-white"
         onClick={handleClick}
       >
         {!nav ? <HiOutlineViewList /> : <HiChevronDoubleUp />}
@@ -18,21 +18,33 @@ const Navbar = () => {
       <ul
         className={
           !nav
-            ? "invisible lg:visible text-white flex  flex-row  justify-center gap-2"
-            : "text-white flex flex-col gap-1 p-2 "
+            ? "invisible lg:visible text-white p-2 flex flex-row justify-between"
+            : "text-white flex flex-col md:gap-1 p-2 "
         }
       >
-        <Link to="/home" className="hover:text-neonBlue">
-          Home
-        </Link>
-        <Link to="/mafia" className="hover:text-neonPink ">
-          Mafia
-        </Link>
-        <Link to="/taboo" className="hover:text-neonBlue ">
-          Taboo
-        </Link>
-        <button className="hover:text-neonPink text-left">Sign In</button>
-        <button className="hover:text-neonBlue text-left">Sign Up</button>
+        <div className="invisible md:visible flex items-center ">
+          <Link className="font-bold mr-4 hidden sm:block" to="/">
+            <span className="text-neonPink text-xl">Game</span>
+            <span className="text-neonBlue text-xl">Night</span>
+          </Link>
+        </div>
+        <div className="md:pr-4 flex flex-col md:inline-block fixed md:relative">
+          <Link to="/home" className="hover:text-neonBlue md:pr-3">
+            Home
+          </Link>
+          <Link to="/mafia" className="hover:text-neonPink md:pr-3">
+            Mafia
+          </Link>
+          <Link to="/taboo" className="hover:text-neonBlue md:pr-3">
+            Taboo
+          </Link>
+          <button className="hover:text-neonPink text-left md:pr-3">
+            Sign In
+          </button>
+          <button className="hover:text-neonBlue text-left md:pr-3">
+            Sign Up
+          </button>
+        </div>
       </ul>
     </>
   );
