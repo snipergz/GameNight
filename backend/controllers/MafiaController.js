fd3cfd69d53dd8688ad2afc1840145fa1347258e
Robert Chapa <robert.chapa8520@gmail.com>
2022-10-14 05:33:00 -0500

    Should be all clear

--- backend/controllers/MafiaController.js
+++ backend/controllers/MafiaController.js
@@ -1,12 +1,21 @@
-const asynchHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler')
 
-const mod = require('../models/modModel')
+const mod = require('../models/GameSessionModel')
+ 
 
 // @desc    Get Server with serverCode
-// @route   Get /gamenight/server/mafia/:id
+// @route   Get /gamenight/server/mafia/:ServerID
 // @access  Public
 const getServer = asyncHandler(async (req, res) => {
-    const server = await mod.find()
+
+    const initserver = await mod.find({ServerID:req.params.ServerID})
+    const server = initserver[0]
+
+    if(!server){
+        res.status(400)
+        throw new Error('Server not found')
+    }
+
     res.status(200).json(server)
 })
 
@@ -14,27 +23,38 @@ const getServer = asyncHandler(async (req, res) => {
 // @route   Post /gamenight/server/mafia
 // @access  Public
 const createServer = asyncHandler(async (req, res) => {
-    if(!req.body.text){
+    if(!req.body){
         res.status(400)
         throw new Error('was empty')
     }
 
     const server = await mod.create({
-        text: req.body.text
+        ServerID: req.body.ServerID,
+        ServerCode: req.body.ServerCode,
+        Status: req.body.Status
     })
 
     res.status(200).json(server)
 })
 
 // @desc    Delete Server with serverCode
-// @route   Delete /gamenight/server/mafia/:id
+// @route   Delete /gamenight/server/mafia/:ServerID
 // @access  Public
 const deleteServer = asyncHandler(async (req, res) => {
 
-    const server = await mod.findById(req.params.id);
-    await mod.remove()
+    const initserver = await mod.find({ServerID:req.params.ServerID})
+    const server = initserver[0]
 
-    res.status(200).json({message: `Deleted Server with server code: ${req.params.id}` })
+    console.log(server)
+
+    if(!server){
+        res.status(400)
+        throw new Error('Server not found')
+    }
+
+    await server.remove()
+
+    res.status(200).json({message: `Deleted Server with server code: ${req.params.ServerID}` })
 })
 
 module.exports = {
--- backend/routes/MafiaRoutes.js
+++ backend/routes/MafiaRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { getServer, createServer, deleteServer } = require('../controllers/MafiaController')
 
 // Mafia Game Server Routes
-router.route('/mafia/:id').get(getServer).delete(deleteServer)
+router.route('/mafia/:ServerID').get(getServer).delete(deleteServer)
 
 router.post('/mafia', createServer)
 
