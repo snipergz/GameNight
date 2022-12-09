bdc86e7b1c2374744a06259c8a00768aec887d5b
Casey Garcia <carlos.garcia26@utrgv.edu>
2022-12-09 13:28:32 -0600

    Added Image to Mystery Game

--- frontend/src/pages/MysteryGame.jsx
+++ frontend/src/pages/MysteryGame.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Choice from '../components/Mystery/MysteryGameChoice'
+import Room from "../assets/Mystery/r.png";
+import Navbar from '../components/Navbar';
 
 const Mystery = () => {
     return (
         <>
+        <Navbar />
+        <div>
+            <img src={Room} alt="Room Image"/>
+        </div>
         <Choice/>
         </>
     )
