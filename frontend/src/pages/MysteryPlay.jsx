1523efe015d34c8dca785a83777c067a69b5ab7e
RobertChapa <36498804+RobertChapa@users.noreply.github.com>
2022-12-10 22:02:06 -0600

    Update MysteryPlay.jsx
    Forgot to correct titling in a spot

--- frontend/src/pages/MysteryPlay.jsx
+++ frontend/src/pages/MysteryPlay.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect} from 'react';
 import axios from 'axios';
 
-function TP(){
+function MysteryPartyGamePage(){
   const [data, setData] = useState([]);
   const [sata, setSata] = useState([]);
   const [loading, setLoading] = useState(true);
