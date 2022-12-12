39b33be6afcd8063608ade4f27f2e0125aa9a6b0
Mike Panuelos <mike.panuelos01@utrgv.edu>
2022-12-12 14:44:46 -0600

    fixing code trying to deploy

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,6 @@ const io = require('socket.io')(8000, {
         origin: ['http://localhost:3000']
     }
 })
-import path from 'path';
-import fs from 'fs';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import Home from '../frontend/src/pages/Home';
 
 const mafiaGameServer = require('../backend/models/MafiaServerModel')
 
@@ -110,25 +105,6 @@ app.use(cors())
 
 app.use('/gamenight/server', require('./routes/MafiaRoutes'), require('./routes/MurderMysteryRoutes'))
 
-app.get('/', (req, res) => {
-    const home = ReactDOMServer.renderToString(<Home />);
-    const indexFile = path.resolve('/index.html');
-  
-    fs.readFile(indexFile, 'utf8', (err, data) => {
-      if (err) {
-        console.error('Something went wrong:', err);
-        return res.status(500).send('Oops, better luck next time!');
-      }
-  
-      return res.send(
-        data.replace('<div id="root"></div>', `<div id="root">${home}</div>`)
-      );
-    });
-});
-
 // Start up the server
-console.log("Javascript running on the server");
-app.listen(process.env.port, function () {  //Updated
-    var addr = app.address();
-    console.log('app listening on http://' + addr.address + ':' + addr.port);
-  });
+// console.log("Javascript running on the server");
+app.listen(port, () => console.log(`Server started on port ${port}`));
