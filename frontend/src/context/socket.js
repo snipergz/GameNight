12ff13a98146e303d0280e194ed6016287c98a22
Mike Panuelos <mike.panuelos01@utrgv.edu>
2022-12-13 18:13:58 -0600

    updating socket.io for deployment

--- frontend/src/context/socket.js
+++ frontend/src/context/socket.js
@@ -2,5 +2,5 @@ import React from "react";
 import socketio from "socket.io-client";
 // import { SOCKET_URL } from "config";
 
-export const socket = socketio('http://localhost:8000')
+export const socket = socketio('https://gamenight-project.herokuapp.com:8000')
 export const SocketContext = React.createContext();
