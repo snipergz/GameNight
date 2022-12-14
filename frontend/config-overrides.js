98acb189cfcee9f34026ba1465f2c8675de69180
Mike Panuelos <mike.panuelos01@utrgv.edu>
2022-12-14 10:59:58 -0600

    attempt 2

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,6 +1,8 @@
 module.exports = function override(config, env) {
     console.log("React app rewired works!")
     config.resolve.fallback = {
+      path: false,
+      os: false,
       fs: false
     };
     return config;
